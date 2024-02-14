a491f5b7c9bef1ccf23f1b84fbb4e481d24264ca
Duy Thanh <tttranduy999@gmail.com>
2024-02-14 13:57:48 +0700

    add banner for chat with bot

--- TestChatGPT/main.go
+++ TestChatGPT/main.go
@@ -4,17 +4,38 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
 
+func printColorText(text string) {
+	fmt.Println("\x1b[36;1m" + text + "\x1b[0m")
+}
+
 func main() {
+	banner, err := os.Open("banner.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer banner.Close()
+
+	scannerbanner := bufio.NewScanner(banner)
+
+	for scannerbanner.Scan() {
+		printColorText(scannerbanner.Text())
+	}
+
+	if err := scannerbanner.Err(); err != nil {
+		fmt.Println("Error: ", err)
+	}
 
 	godotenv.Load(".env")
 	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 	scanner := bufio.NewScanner(os.Stdin)
+
 	var message []openai.ChatCompletionMessage
 	fmt.Print("\x1b[35muser> \x1b[0m")
 	for scanner.Scan() {
